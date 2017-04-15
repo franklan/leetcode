183a7194d39b70c0bc89f4332cab0d01a7f60551
tlan <lanty1988@gmail.com>
2017-04-15 13:54:04 +0800

    123

--- 123_Best_Time_to_Buy_and_Sell_Stock_III/solution.h
+++ 123_Best_Time_to_Buy_and_Sell_Stock_III/solution.h
@@ -6,11 +6,13 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-        // f[k, ii] represents the max profit up until prices[ii] (Note: NOT ending with prices[ii]) using at most k transactions. 
-        // f[k, ii] = max(f[k, ii-1], prices[ii] - prices[jj] + f[k-1, jj]) { jj in range of [0, ii-1] }
-        //          = max(f[k, ii-1], prices[ii] + max(f[k-1, jj] - prices[jj]))
-        // f[0, ii] = 0; 0 times transation makes 0 profit
-        // f[k, 0] = 0; if there is only one price data point you can't make any money no matter how many times you can trade
+        // f[k, ii] = get maxProfix until ii days with k transactions
+        // f[0, ii] = 0
+        // f[k, 0] = 0
+        // f[k, ii] = std::max(f[k, ii-1], f[k-1, jj] + prices[ii] - prices[jj]) { jj in range of [0, ii-1] }
+        //               = max(f[k, ii-1], prices[ii] + max(f[k-1, jj] - prices[jj]))
+        // this is a mathematical derivation. we can get max(f[k-1, jj] - prices[jj]) in the loop
+        
         if (prices.size() <= 1) return 0;
         else {
             int K = 2; // number of max transation allowed
